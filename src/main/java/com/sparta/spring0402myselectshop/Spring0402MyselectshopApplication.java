0345b17fdd7682306904523d503c515251ae1204
mine <mineki94@naver.com>
2024-02-05 01:59:37 +0900

    init commit

---
+++ src/main/java/com/sparta/spring0402myselectshop/Spring0402MyselectshopApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring0402myselectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0402MyselectshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring0402MyselectshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spring0402myselectshop/Spring0402MyselectshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring0402myselectshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring0402MyselectshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
