ee409404aa5860d02abea45e82ce2b3c317c30be
mine <mineki94@naver.com>
2024-02-05 20:11:29 +0900

    상품 폴더 설계

---
+++ src/main/java/com/sparta/spring0402myselectshop/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.spring0402myselectshop.repository;
+
+import com.sparta.spring0402myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder,Long> {
+}
---
+++ src/main/java/com/sparta/spring0402myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.spring0402myselectshop.repository;
+
+import com.sparta.spring0402myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+}
